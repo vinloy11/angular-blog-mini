311ee6843ed39d09680c3bb9539b283d9bc24dbc
Shtork A.O <shtork@prodv.net>
2020-07-08 22:44:32 +0700

    #3 Added firebace and authorization

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,6 +7,8 @@ import { DashboardPageComponent } from './dashboard-page/dashboard-page.componen
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AuthService} from './shared/services/auth.service';
+import {SharedModule} from './shared/shared.module';
 
 @NgModule({
   declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent],
@@ -14,6 +16,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
     RouterModule.forChild([
       {
         path: '', component: AdminLayoutComponent, children: [
@@ -28,6 +31,9 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
   ],
   exports: [
     RouterModule,
+  ],
+  providers: [
+    AuthService
   ],
 })
 export class AdminModule {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyBPCqD8HM32qTZOdNPt9vLYh-v2YbPdOXo',
 };
