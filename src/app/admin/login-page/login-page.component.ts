868486919184adfa6e0b1bbde2a32b1727868ee8
Shtork A.O <shtork@prodv.net>
2020-07-10 23:17:16 +0700

    #4 Added admin panel guard

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {User} from '../../shared/interfaces';
 import {AuthService} from '../shared/services/auth.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -12,13 +12,22 @@ import {Router} from '@angular/router';
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
+  submitted = false;
+  message: string;
 
   constructor(
-    private auth: AuthService,
+    public auth: AuthService,
     private router: Router,
+    private route: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      const key = 'loginAgain';
+      if (params[key]) {
+        this.message = 'Пожалуйста, введите данные';
+      }
+    });
     this.form = new FormGroup({
       email: new FormControl(null, [
         Validators.required, Validators.email
@@ -35,6 +44,8 @@ export class LoginPageComponent implements OnInit {
       return;
     }
 
+    this.submitted = true;
+
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
@@ -43,6 +54,9 @@ export class LoginPageComponent implements OnInit {
     this.auth.login(user).subscribe(() => {
       this.form.reset();
       this.router.navigate(['/admin', 'dashboard']);
+      this.submitted = false;
+    }, () => {
+      this.submitted = false;
     });
   }
 }
--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,25 +1,58 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {FbAuthResponse, User} from '../../../shared/interfaces';
-import {Observable} from 'rxjs';
+import {Observable, Subject, throwError} from 'rxjs';
 import {environment} from '../../../../environments/environment';
-import {tap} from 'rxjs/operators';
+import {catchError, tap} from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
+  public error$: Subject<string> = new Subject<string>();
   constructor(private http: HttpClient) {}
   get token(): string {
-    return '';
+    const expDate = new Date(localStorage.getItem('fb-token-exp'));
+    if (new Date() > expDate) {
+      this.logout();
+      return null;
+    }
+    return localStorage.getItem('fb-token');
   }
   login(user: User): Observable<any> {
+    user.returnSecureToken = true;
     return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
       .pipe(
-        tap(this.setToken)
+        tap(this.setToken),
+        catchError(this.handleError.bind(this))
       );
   }
-  logout() {}
+  logout() {
+    this.setToken(null);
+  }
   isAuthenticated(): boolean {
     return !!this.token;
   }
-  private setToken(response: FbAuthResponse) {}
+  protected handleError(error: HttpErrorResponse) {
+    const {message} = error.error.error;
+    switch (message) {
+      case 'EMAIL_NOT_FOUND':
+        this.error$.next('Такого email нет');
+        break;
+      case 'INVALID_EMAIL':
+        this.error$.next('Неверный email');
+        break;
+      case 'INVALID_PASSWORD':
+        this.error$.next('Неверный пароль');
+        break;
+    }
+    return throwError(error);
+  }
+  protected setToken(response: FbAuthResponse | null ): void {
+    if (response) {
+      const expDate = new Date(new Date().getTime() + parseInt(response.expiresIn, 10) * 1000);
+      localStorage.setItem('fb-token', response.idToken);
+      localStorage.setItem('fb-token-exp', expDate.toString());
+    } else {
+      localStorage.clear();
+    }
+  }
 }
